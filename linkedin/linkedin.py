97364f5e994429882ef8e4060aeb9bead9dde4f3
Nazmi ZORLU <nazmizorlu@gmail.com>
2015-11-06 22:02:07 +0300

    Get Network Shares Comments

--- linkedin/linkedin.py
+++ linkedin/linkedin.py
@@ -551,3 +551,14 @@ class LinkedInApplication(object):
         response = self.make_request('GET', url, params=params, headers=headers)
         raise_for_error(response)
         return response.json()
+
+    def get_share_comments(self, post_id, selectors=None, params=None,
+                           headers=None):
+        url = '%s/~/network/updates/key=%s/update-comments' % (ENDPOINTS.PEOPLE,
+                                                               post_id)
+        if selectors:
+            url = '%s:(%s)' % (url, LinkedInSelector.parse(selectors))
+
+        response = self.make_request('GET', url, params=params, headers=headers)
+        raise_for_error(response)
+        return response.json()
