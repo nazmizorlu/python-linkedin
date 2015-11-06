faf6531ed4e899889a3075df1c29f36b7be6c526
Nazmi ZORLU <nazmizorlu@gmail.com>
2015-11-06 22:13:39 +0300

    Commenting as company.

--- linkedin/linkedin.py
+++ linkedin/linkedin.py
@@ -572,3 +572,11 @@ class LinkedInApplication(object):
         response = self.make_request('GET', url, params=params, headers=headers)
         raise_for_error(response)
         return response.json()
+
+    def comment_as_company(self, company_id, update_key, comment):
+        comment = {'comment': comment}
+        url = '%s/updates/key=%s/update-comments-as-company' % (
+            ENDPOINTS.COMPANIES, company_id, update_key)
+        response = self.make_request('PUT', url, data=json.dumps(comment))
+        raise_for_error(response)
+        return True
